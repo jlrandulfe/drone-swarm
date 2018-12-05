1fd302f485dd58b7edd1f81a57f22dc67a29d56c
jlrandulfe <jlrandulfe@gmail.com>
2018-12-05 18:44:19 +0100

    Add logic for parsing incoming messages and sending error messages
    A new module in the error_estimator package deals with the
    serialization and deserialization of the MultiArray into numpy
    arrays, and viceversa.
    
    The publishers and subscribers for interfacing the pattern
    generator, the kalman filter, and the controller are set up in
    the code.

--- catkin_ws/src/error_estimator/src/error_estimator/error_estimator.py
+++ catkin_ws/src/error_estimator/src/error_estimator/error_estimator.py
@@ -4,20 +4,52 @@ Error estimator top level module
 """
 # Standard libraries
 # Third-party libraries
+import numpy as np
 import rospy
 import geometry_msgs.msg
 import std_msgs.msg
 # Local libraries
+from error_estimator import array_operations
 
 
 class ErrorEstimatorNode():
 
     def __init__(self):
+        # Errors matrix
+        self.errors = np.array([[0, 1, 2],[1, 0, 3],[2, 3, 0]])
+        self.predicted_distances = np.array()
+        self.desired_distances = np.array()
+
+        # Kalman filter topic susbscribers
+        rospy.Subscriber("kalman/state", std_msgs.msg.Float64MultiArray,
+                         self.kalman_callback, queue_size=1)
+        # Pattern generator topic susbscribers
+        rospy.Subscriber("pattern_generator/start",
+                         std_msgs.msg.Float64MultiArray,
+                         self.pat_gen_start_callback, queue_size=1)
+
+        # Drone controller topic publishers
+        self.errors_pub = rospy.Publisher(
+                "error_estimator/errors",
+                std_msgs.msg.Float64MultiArray,
+                queue_size=1)
+        return
+
+    def kalman_callback(self, data):
+        self.predicted_distances = array_operations.multiarray2np(data)
+        return
+
+    def pat_gen_start_callback(self, data):
+        self.desired_distances = array_operations.multiarray2np(data)
         return
 
     def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.errors_pub.publish(array_operations.np2multiarray(self.errors))
+            rate.sleep()
         return
-    
+
 
 def main():
     # Instantiate the error_estimator node class and run it
