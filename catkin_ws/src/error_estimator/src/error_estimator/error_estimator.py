0f74f0266d679a9fcdf6420caacbe10253772f92
jlrandulfe <jlrandulfe@gmail.com>
2018-12-05 15:25:45 +0100

    Error estimator package skeleton

---
+++ catkin_ws/src/error_estimator/src/error_estimator/error_estimator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Error estimator top level module
+"""
+# Standard libraries
+# Third-party libraries
+import rospy
+import geometry_msgs.msg
+import std_msgs.msg
+# Local libraries
+
+
+class ErrorEstimatorNode():
+
+    def __init__(self):
+        return
+
+    def run(self):
+        return
+    
+
+def main():
+    # Instantiate the error_estimator node class and run it
+    error_estimator = ErrorEstimatorNode()
+    error_estimator.run()
+    return
