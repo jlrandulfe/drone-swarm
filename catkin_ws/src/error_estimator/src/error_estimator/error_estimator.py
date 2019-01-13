07c14d3c95889b30e4486d29f0ee6bc78fbb4858
Unknown <mortp14@student.sdu.dk>
2019-01-13 16:06:04 +0100

    added controller calculations to error_estimator

--- catkin_ws/src/error_estimator/src/error_estimator/error_estimator.py
+++ catkin_ws/src/error_estimator/src/error_estimator/error_estimator.py
@@ -12,6 +12,9 @@ import std_msgs.msg
 from error_estimator import array_operations
 from error_estimator import error_functions
 
+def dist(vector):
+    length = np.sqrt(pow(vector[0],2)+pow(vector[1],2))
+    return length
 
 class ErrorEstimatorNode():
 
@@ -19,8 +22,8 @@ class ErrorEstimatorNode():
         self.start = False
         # Errors matrix
         self.errors = np.array([[0, 1, 2],[1, 0, 3],[2, 3, 0]])
-        self.predicted_distances = np.array()
-        self.desired_distances = np.array()
+        self.predicted_distances = np.array([])
+        self.desired_distances = np.array([])
 
         # Kalman filter topic susbscribers
         rospy.Subscriber("kalman/state", std_msgs.msg.Float64MultiArray,
@@ -31,17 +34,41 @@ class ErrorEstimatorNode():
                          self.pat_gen_start_callback, queue_size=1)
 
         # Drone controller topic publishers
-        self.errors_pub = rospy.Publisher(
-                "error_estimator/errors",
+        #self.errors_pub = rospy.Publisher(
+         #       "error_estimator/errors",
+          #      std_msgs.msg.Float64MultiArray,
+           #     queue_size=1)
+        self.control_variable_pub = rospy.Publisher(
+                "error_estimator/control_value",
                 std_msgs.msg.Float64MultiArray,
                 queue_size=1)
+        
         return
 
     def kalman_callback(self, data):
         self.predicted_distances = array_operations.multiarray2np(data)
         self.errors = error_functions.simple_differences(
                 self.desired_distances, self.predicted_distances)
-        self.errors_pub.publish(array_operations.np2multiarray(self.errors))
+        
+
+        #kalman P gain
+        Kp = 1.2
+        units = np.zeros([1,2])
+
+        drone = 2 #must be changed to fit with actual drone
+
+        for j in range(0, len(self.errors)):
+            if(self.errors[drone-1,j] != 0.0):
+            #this is the row of the drone i am
+
+                #add op the unit vector*e of all in this row
+                if drone-1 > j:
+                    units = units - (self.desired_distances[drone-1,j]/dist(self.desired_distances[drone-1,j]))*self.errors[drone-1,j]
+                elif drone-1 < j:
+                    units = units + (self.desired_distances[drone-1,j]/dist(self.desired_distances[drone-1,j]))*self.errors[drone-1,j]
+
+        self.control_variable_pub.publish(array_operations.np2multiarray(Kp*units))
+        #self.errors_pub.publish(array_operations.np2multiarray(self.errors))
         return
 
     def pat_gen_start_callback(self, data):
